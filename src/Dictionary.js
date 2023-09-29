7ead8f0f29d6bbb832d839fe610bb4f2c7410890
ycode2023 <yaldakesh@gmail.com>
2023-09-29 14:49:50 +0100

    installed axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
  import React, {useState} from "react";
+ import axios from "axios";
  import "./Dictionary.css";
 
 
@@ -6,9 +7,17 @@
 let [keyword, setKeyword] = useState("");
 
 
+function handleRespone(response){
+    console.log(response.data[0]);
+}
+
+
 function search(event){
     event.preventDefault();
     alert(`Defining ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleRespone);
 }
 
 function handleKeywordChange(event){
